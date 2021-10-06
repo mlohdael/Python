6ba7c166acfde620ec236b6a634f792f196f93d5
Christopher Leadholm <c.leadholm@gmail.com>
2021-10-05 20:10:56 -0400

    required spot i had to type out
    idk whi it makes me type shit out to type stuff out. dumb.

--- Factorial.py
+++ Factorial.py
@@ -1,9 +1,29 @@
-def factorial(num: int):
-       if num < 0:
-              print("Factorial can't be calculated for numbers less than zero.")
-       elif num == 0:
-              print("Factorial of 0 is 1.")
-       else:
-              print(f"Factorial of {num} is {num*factorial(num-1)}.")
-n = int(input("Enter a number to find factorial: "))
-factorial(n)
+import math
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+n = int(input("Input a number to compute the factiorial : "))
+print(factorial(n))
+
+"""
+Method 2:
+Here we are going to use in-built fuction for factorial which is provided by Python for
+user conveniance.
+
+Steps:
+     -For this you should import math module first
+     -and use factorial() method from math module
+     
+Note:
+    Appear error when pass a negative or fraction value in factorial() method, so plz refrain from this.
+
+Let's code it:
+"""
+if n>=0 :
+    print(math.factorial(n))
+else:
+    print("Value of n is inValid!")
